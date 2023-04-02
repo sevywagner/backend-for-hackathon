4836e36cf0332afe3560d2769914c64548f1522a
sevywagner <73050647+sevywagner@users.noreply.github.com>
2023-04-02 09:05:38 -0500

    fixes

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator')
 const Order = require('../models/Order');
+const TimeStamp = require('../models/TimeStamp');
 
 exports.postOrder = (req, res, next) => {
     const name = req.body.name;
@@ -19,7 +20,11 @@ exports.postOrder = (req, res, next) => {
     }
 
     const order = new Order(name, address, email, date, time, bloodAmount, payout);
+    const timeStamp = new TimeStamp(date, time);
+
     order.save().then(() => {
+        return timeStamp.save();
+    }).then(() => {
         res.status(201).json({
             message: 'Successfully placed order'
         });
